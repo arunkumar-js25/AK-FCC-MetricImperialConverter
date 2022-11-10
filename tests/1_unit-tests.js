5039a8cceccc8ca751530b447ddb7e90b3b87005
Arun Kumar <53009824+arunkumar-js25@users.noreply.github.com>
2022-11-10 05:24:52 +0000

    21 test cases created and successfully passed

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -14,7 +14,50 @@ suite('Unit Tests', function(){
   test('read a fractional input', function () {
 		  assert.equal(convertHandler.getNum("2/5gal"), '0.4');
 		});
+  test('read a fractional input', function () {
+		  assert.equal(convertHandler.getNum("2.3/5gal"), '0.45999999999999996');
+		});
   test('return an error on a double-fraction', function () {
 		  assert.equal(convertHandler.getNum("2/5/6gal"), 'invalid number');
+		});
+  test('default to a numerical input of 1', function () {
+		  assert.equal(convertHandler.getNum(""), '1');
+		});
+  test('read each valid input unit', function () {
+		  assert.equal(convertHandler.getUnit("1l"), 'L');
+		});
+  test('return an error for an invalid input unit', function () {
+		  assert.equal(convertHandler.getUnit("1le"), 'invalid unit');
+		});
+  test('correct return unit for each valid input unit', function () {
+		  assert.equal(convertHandler.getReturnUnit("L"), 'gal');
+		});
+
+  test('spelled-out string unit for each valid input unit', function () {
+		  assert.equal(convertHandler.spellOutUnit("L"), 'litres');
+		});
+
+  //GAL TO L
+  test('gal to L', function () {
+		  assert.equal(convertHandler.convert("10","gal"), 37.8541);
+		});
+  test('L to gal', function () {
+		  assert.equal(convertHandler.convert("37.8541","L"), 10);
+		});
+
+  //KM TO MI
+  test('mi to km', function () {
+		  assert.equal(convertHandler.convert("10","mi"), 16.0934);
+		});
+  test('km to mi', function () {
+		  assert.equal(convertHandler.convert("16.0934","km"), 10);
+		});
+
+  //lbs TO kg
+  test('lbs to kg', function () {
+		  assert.equal(convertHandler.convert("10","lbs"), 4.53592);
+		});
+  test('kg to lbs', function () {
+		  assert.equal(convertHandler.convert("4.53592","kg"), 10);
 		});
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,53 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+  test('Convert a valid input', function (done) {
+		  chai.request(server)
+      .get('/api/convert')
+      .query({input:'10L'})
+      .end(function (err, res) {
+			  assert.equal(res.status, 200);
+			  done();
+			});
+		});
 
+  test('Convert an invalid input', function (done) {
+		  chai.request(server)
+      .get('/api/convert')
+      .query({input:'32g'})
+      .end(function (err, res) {
+			  assert.equal(res.status, 200);
+			  done();
+			});
+		});
+
+  test('Convert an invalid number', function (done) {
+		  chai.request(server)
+      .get('/api/convert')
+      .query({input:'3/7.2/4kg'})
+      .end(function (err, res) {
+			  assert.equal(res.status, 200);
+			  done();
+			});
+		});
+
+  test('Convert an invalid number AND unit ', function (done) {
+		  chai.request(server)
+      .get('/api/convert')
+      .query({input:'3/7.2/4kilomegagram'})
+      .end(function (err, res) {
+			  assert.equal(res.status, 200);
+			  done();
+			});
+		});
+
+  test('Convert with no number', function (done) {
+		  chai.request(server)
+      .get('/api/convert')
+      .query({input:'kg'})
+      .end(function (err, res) {
+			  assert.equal(res.status, 200);
+			  done();
+			});
+		});
 });
